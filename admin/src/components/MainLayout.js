317e730d345d9763143a6770d8457ad19858dc6a
francolivelli <franco.livelli@gmail.com>
2023-07-21 23:08:56 -0300

    sider

--- admin/src/components/MainLayout.js
+++ admin/src/components/MainLayout.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { Layout, Menu, Button, theme } from "antd";
+import {
+  AiOutlineDashboard,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+  AiOutlineBgColors,
+} from "react-icons/ai";
+import { SiBrandfolder } from "react-icons/si";
+import { BiCategoryAlt } from "react-icons/bi";
+import { FaClipboardList, FaBloggerB } from "react-icons/fa";
+import { ImBlog } from "react-icons/im";
+import { TbMessage2Question } from "react-icons/tb";
+import { useNavigate, Outlet } from "react-router-dom";
+
 const { Header, Sider, Content } = Layout;
 
 const MainLayout = () => {
@@ -14,29 +21,121 @@ const MainLayout = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+  const navigate = useNavigate();
+
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
+        <div className="logo">
+          <h2 className="text-white fs-5 text-center py-3">
+            <span className="sm-logo">D</span>
+            <span className="lg-logo">Digistore</span>
+          </h2>
+        </div>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={["1"]}
+          defaultSelectedKeys={[""]}
+          onClick={({ key }) => {
+            if (key === "signout") {
+            } else {
+              navigate(key);
+            }
+          }}
           items={[
             {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "nav 1",
+              key: "",
+              icon: <AiOutlineDashboard className="fs-4" />,
+              label: "Panel",
             },
             {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "nav 2",
+              key: "customers",
+              icon: <AiOutlineUser className="fs-4" />,
+              label: "Clientes",
             },
             {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "nav 3",
+              key: "catalog",
+              icon: <AiOutlineShoppingCart className="fs-4" />,
+              label: "Catálogo",
+              children: [
+                {
+                  key: "product",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Productos",
+                },
+                {
+                  key: "product-list",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Lista de productos",
+                },
+                {
+                  key: "brand",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Marcas",
+                },
+                {
+                  key: "brand-list",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Lista de marcas",
+                },
+                {
+                  key: "category",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Categorías",
+                },
+                {
+                  key: "category-list",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Lista de categorías",
+                },
+                {
+                  key: "color",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Colores",
+                },
+                {
+                  key: "color-list",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Lista de colores",
+                },
+              ],
+            },
+            {
+              key: "orders",
+              icon: <FaClipboardList className="fs-4" />,
+              label: "Pedidos",
+            },
+            {
+              key: "blogs",
+              icon: <FaBloggerB className="fs-4" />,
+              label: "Blogs",
+              children: [
+                {
+                  key: "blog",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Crear blog",
+                },
+                {
+                  key: "blog-list",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Lista de blogs",
+                },
+                {
+                  key: "blog-category",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Crear categoría",
+                },
+                {
+                  key: "blog-category-list",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Lista de categorías",
+                },
+              ],
+            },
+            {
+              key: "enquiries",
+              icon: <TbMessage2Question className="fs-4" />,
+              label: "Consultas",
             },
           ]}
         />
@@ -65,7 +164,7 @@ const MainLayout = () => {
             minHeight: 280,
             background: colorBgContainer,
           }}>
-          Content
+          <Outlet />
         </Content>
       </Layout>
     </Layout>

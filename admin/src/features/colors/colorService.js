73e527727221717fe634b09552c07270390357cd
francolivelli <franco.livelli@gmail.com>
2023-08-17 12:52:37 -0300

    api integration
    add color

--- admin/src/features/colors/colorService.js
+++ admin/src/features/colors/colorService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { config } from "../../utils/axiosconfig";
 import { base_url } from "../../utils/base_url";
 
 const getColors = async () => {
@@ -7,8 +8,15 @@ const getColors = async () => {
   return response.data;
 };
 
+const createColor = async (color) => {
+  const response = await axios.post(`${base_url}colors/`, color, config);
+
+  return response.data;
+};
+
 const colorService = {
   getColors,
+  createColor,
 };
 
 export default colorService;
--- admin/src/pages/Addcolor.js
+++ admin/src/pages/Addcolor.js
@@ -1,16 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CustomInput from "../components/CustomInput";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import { createColor } from "../features/colors/colorSlice";
+
+let schema = Yup.object().shape({
+  title: Yup.string().required("Ingresá un color"),
+});
 
 const Addcolor = () => {
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const newColor = useSelector((state) => state.color);
+
+  const { isSuccess, isError, isLoading, createdColor } = newColor;
+
+  useEffect(() => {
+    if (isSuccess && createdColor) {
+      toast.success("¡Color agregado!");
+    }
+    if (isError) {
+      toast.error("Algo salió mal");
+    }
+  }, [isSuccess, isError, isLoading, createdColor]);
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      dispatch(createColor(values));
+      formik.resetForm();
+      setTimeout(() => {
+        navigate("/admin/color-list");
+      }, 3000);
+    },
+  });
   return (
     <div>
       <h3 className="mb-4 title">Agregar color</h3>
       <div>
-        <form action="">
-          <CustomInput type="color" label="Color" />
+        <form
+          action=""
+          onSubmit={formik.handleSubmit}
+          className="d-flex gap-2 flex-column">
+          <CustomInput
+            type="color"
+            label="Color"
+            name="title"
+            onCh={formik.handleChange("title")}
+            onBl={formik.handleBlur("title")}
+            val={formik.values.title}
+            id="color"
+          />
+          <div className="error">
+            {formik.touched.title && formik.errors.title}
+          </div>
           <button
             type="sumit"
-            className="btn btn-success border-0 rounded-3 my-4">
+            className="btn btn-success border-0 rounded-3 my-4"
+            style={{ width: "fit-content" }}>
             Agregar color
           </button>
         </form>
